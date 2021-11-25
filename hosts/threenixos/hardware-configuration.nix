f7820563f1bb4bf084b2065b1084684522abdd77
Siyuan Wang <c.one@thrimbda.com>
2021-11-25 21:05:42 +0800

    :sparkles: git config

--- hosts/threenixos/hardware-configuration.nix
+++ hosts/threenixos/hardware-configuration.nix
@@ -8,19 +8,14 @@
     [ (modulesPath + "/installer/scan/not-detected.nix")
     ];
 
-  boot.initrd.availableKernelModules = [ "nvme" "xhci_pci" "ahci" "usbhid" "usb_storage" "sd_mod" ];
+  boot.initrd.availableKernelModules = [ "nvme" "xhci_pci" "ahci" "usb_storage" "usbhid" "sd_mod" ];
   boot.initrd.kernelModules = [ ];
   boot.kernelModules = [ "kvm-amd" ];
   boot.extraModulePackages = [ ];
 
   fileSystems."/" =
-    { device = "/dev/disk/by-uuid/3049bfda-3901-468f-97b7-902b70a4132c";
+    { device = "/dev/disk/by-uuid/cf08650d-428d-48cf-90f9-6d6db253e038";
       fsType = "ext4";
-    };
-
-  fileSystems."/boot" =
-    { device = "/dev/disk/by-uuid/464D-942C";
-      fsType = "vfat";
     };
 
   swapDevices = [ ];
