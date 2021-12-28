cec9e08a15282666708f3df72842334e97b1b56f
Siyuan Wang <c.one@thrimbda.com>
2021-12-28 17:50:17 +0800

    :sparkles: make input method modules

---
+++ modules/desktop/input/colemak.nix
@@ -0,0 +1,19 @@
+{ options, config, lib, pkgs, ... }:
+
+with lib;
+with lib.my;
+let cfg = config.modules.desktop.input.colemak;
+in {
+  options.modules.desktop.input.colemak = {
+    enable = mkBoolOpt false;
+  };
+
+  config = mkIf cfg.enable {
+    services.xserver = {
+      layout = "us";
+      xkbVariant = "colemak";
+      enable = true;
+    };
+    console.useXkbConfig = true;
+  };
+}
