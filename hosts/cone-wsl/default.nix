5076cb7c4dd930890c42923315b4078a60c11ff1
Siyuan Wang <c.one@thrimbda.com>
2022-05-29 18:49:19 +0800

    feat: add new wsl host

---
+++ hosts/cone-wsl/default.nix
@@ -0,0 +1,122 @@
+{ pkgs, config, lib, ... }:
+
+with lib;
+let
+  nixos-wsl = import ./nixos-wsl;
+in
+{
+  imports = [
+    ../home.nix
+    #./hardware-configuration.nix
+    nixos-wsl.nixosModules.wsl
+  ];
+
+  boot = {
+    loader = {
+      efi.canTouchEfiVariables = mkDefault true;
+      systemd-boot.configurationLimit = 10;
+      systemd-boot.enable = false;
+    };
+  };
+
+  wsl = {
+    enable = true;
+    automountPath = "/mnt";
+    defaultUser = "c1";
+    startMenuLaunchers = true;
+
+    # Enable integration with Docker Desktop (needs to be installed)
+    # docker.enable = true;
+  };
+  ## Modules
+  modules = {
+    dev = {
+      # cc.enable = true;
+      go.enable = true;
+      node.enable = true;
+      rust.enable = true;
+      python.enable = true;
+      scala.enable = true;
+    };
+    editors = {
+      default = "nvim";
+      # emacs.enable = true;
+      vim.enable = true;
+    };
+    shell = {
+      adl.enable = true;
+      # vaultwarden.enable = true;
+      direnv.enable = true;
+      git.enable    = true;
+      gnupg.enable  = true;
+      tmux.enable   = true;
+      zsh.enable    = true;
+    };
+    services = {
+      ssh.enable = true;
+      # docker.enable = true;
+      # onedrive.enable = true;
+      k8s.enable = true;
+      # Needed occasionally to help the parental units with PC problems
+      # teamviewer.enable = true;
+    };
+    theme.active = "alucard";
+  };
+
+
+  ## Local config
+  programs.ssh.startAgent = true;
+  services.openssh.startWhenNeeded = true;
+
+  # networking.networkmanager.enable = true;
+  # The global useDHCP flag is deprecated, therefore explicitly set to false
+  # here. Per-interface useDHCP will be mandatory in the future, so this
+  # generated config replicates the default behaviour.
+  # networking.useDHCP = false;
+
+  time.timeZone = "Asia/Shanghai";
+
+
+  ## Personal backups
+  # Syncthing is a bit heavy handed for my needs, so rsync to my NAS instead.
+  # systemd = {
+  #   services.backups = {
+  #     description = "Backup /usr/store to NAS";
+  #     wants = [ "usr-drive.mount" ];
+  #     path  = [ pkgs.rsync ];
+  #     environment = {
+  #       SRC_DIR  = "/usr/store";
+  #       DEST_DIR = "/usr/drive";
+  #     };
+  #     script = ''
+  #       rcp() {
+  #         if [[ -d "$1" && -d "$2" ]]; then
+  #           echo "---- BACKUPING UP $1 TO $2 ----"
+  #           rsync -rlptPJ --chmod=go= --delete --delete-after \
+  #               --exclude=lost+found/ \
+  #               --exclude=@eaDir/ \
+  #               --include=.git/ \
+  #               --filter=':- .gitignore' \
+  #               --filter=':- $XDG_CONFIG_HOME/git/ignore' \
+  #               "$1" "$2"
+  #         fi
+  #       }
+  #       rcp "$HOME/projects/" "$DEST_DIR/projects"
+  #       rcp "$SRC_DIR/" "$DEST_DIR"
+  #     '';
+  #     serviceConfig = {
+  #       Type = "oneshot";
+  #       Nice = 19;
+  #       IOSchedulingClass = "idle";
+  #       User = config.user.name;
+  #       Group = config.user.group;
+  #     };
+  #   };
+  #   timers.backups = {
+  #     wantedBy = [ "timers.target" ];
+  #     partOf = [ "backups.service" ];
+  #     timerConfig.OnCalendar = "*-*-* 00,12:00:00";
+  #     timerConfig.Persistent = true;
+  #   };
+  # };
+}
