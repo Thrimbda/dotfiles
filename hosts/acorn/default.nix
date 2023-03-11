35170347e205d53bb961e5d7c66558da36f2b2ec
Siyuan Wang <c.one@thrimbda.com>
2023-03-11 13:27:44 +0800

    feat(acorn): setup acorn on azure

--- hosts/acorn/default.nix
+++ hosts/acorn/default.nix
@@ -1,16 +1,17 @@
-{ pkgs, config, lib, ... }:
+{ pkgs, modulesPath, config, lib, ... }:
 {
   imports = [
     ../home.nix
     ../server.nix
-    ./hardware-configuration.nix
+    "${modulesPath}/virtualisation/azure-common.nix"
+    # ./hardware-configuration.nix
   ];
 
   ## Modules
   modules = {
     dev = {
       # cc.enable = true;
-      go.enable = true;
+      # go.enable = true;
       node.enable = true;
       rust.enable = true;
       python.enable = true;
@@ -33,12 +34,9 @@
     services = {
       ssh.enable = true;
       docker.enable = true;
-      # onedrive.enable = true;
-      # k8s.enable = true;
-      # Needed occasionally to help the parental units with PC problems
-      # teamviewer.enable = true;
     };
     theme.active = "alucard";
+    theme.useX = false;
   };
 
   ## Local config
@@ -49,8 +47,19 @@
   # The global useDHCP flag is deprecated, therefore explicitly set to false
   # here. Per-interface useDHCP will be mandatory in the future, so this
   # generated config replicates the default behaviour.
-  # networking.useDHCP = false;
+  networking.useDHCP = true;
 
   time.timeZone = "Asia/Shanghai";
+  networking.hostName = "acorn";
+  networking.dhcpcd.enable = true;
+  
+  systemd.services."serial-getty@ttyS0".enable = lib.mkForce true;
+  systemd.services."serial-getty@hvc0".enable = lib.mkForce true;
+  systemd.services."getty@tty1".enable = lib.mkForce true;
+  systemd.services."autovt@".enable = lib.mkForce true;
 
+  fileSystems."/boot" = {
+    device = "/dev/disk/by-label/ESP";
+    fsType = "vfat";
+  };
 }
